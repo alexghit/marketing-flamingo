665b3d7816b55f212993714af5c078d37943bf09
Alex Ghit <76708587+alexghit@users.noreply.github.com>
2025-11-23 05:59:03 +0200

    Update and rename loader to loader.js

---
+++ src/loader.js
@@ -0,0 +1,4 @@
+src="https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/gsap.min.js"
+src="https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/CustomEase.min.js
+src="https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/SplitText.min.js
+
